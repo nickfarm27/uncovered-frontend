60205ac30224e172dd36774126f7e424bd187b24
Nicholas Farm <nicholasfarm27@gmail.com>
2022-01-29 15:51:34 +0800

    Commit left out file

---
+++ src/components/auth/LoggedIn.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { get_Auth } from "../../Firebase";
+
+type Props = { children: JSX.Element };
+
+const LoggedIn = (props: Props) => {
+    const auth = get_Auth;
+    const location = useLocation();
+
+    if (auth.currentUser) {
+        return <Navigate to={location.state ? location.state as string : "/"} replace={true} />;
+    }
+
+    return props.children;
+};
+
+export default LoggedIn;
